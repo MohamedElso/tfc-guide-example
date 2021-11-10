0ffe353cc1cc202290734f48cf304fb4c40ff54a
MS <mohammed.elsodany@gmail.com>
2021-11-10 16:00:39 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ data "aws_ami" "amazon_linux" {
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    values = ["amzn2-ami-hvm-2.0.20211005.0-x86_64-gp2"]
   }
 
   owners = ["amazon"] # Canonical
